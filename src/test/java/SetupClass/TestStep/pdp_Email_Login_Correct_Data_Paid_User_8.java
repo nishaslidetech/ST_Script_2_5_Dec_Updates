2d20eb5f0f4a301197dfcec1e5f44757056b8ee7
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-11-30 11:12:49 +0530

    Update pdp_Email_Login_Correct_Data_Paid_User_8.java

--- src/test/java/SetupClass/TestStep/pdp_Email_Login_Correct_Data_Paid_User_8.java
+++ src/test/java/SetupClass/TestStep/pdp_Email_Login_Correct_Data_Paid_User_8.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-
+import org.junit.Assert;
 import SetupClass.Set;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
