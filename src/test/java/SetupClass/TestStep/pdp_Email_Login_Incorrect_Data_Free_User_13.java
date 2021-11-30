07d642e5b7917c9764aa213ffc348dc2c37580b9
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-11-30 12:07:22 +0530

    Update pdp_Email_Login_Incorrect_Data_Free_User_13.java

--- src/test/java/SetupClass/TestStep/pdp_Email_Login_Incorrect_Data_Free_User_13.java
+++ src/test/java/SetupClass/TestStep/pdp_Email_Login_Incorrect_Data_Free_User_13.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-
+import org.junit.Assert;
 import SetupClass.Set;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -17,7 +17,8 @@ public class pdp_Email_Login_Incorrect_Data_Free_User_13 extends Set {
 	
 	@Given("^user is already on PDP Page NA xiii$")
 	public void user_is_already_on_PDP_Page_NA_xiii() throws Throwable {
-		
+		 driver.manage().deleteAllCookies();
+		Thread.sleep(2000);
 		driver.get("https://www.slideteam.net/papa-johns-company-profile-overview-financials-and-statistics-from-2014-2018.html");
 		Thread.sleep(3000);
 		
@@ -41,17 +42,17 @@ public class pdp_Email_Login_Incorrect_Data_Free_User_13 extends Set {
 
 	@Then("^user enter incorrect details to login xiii$")
 	public void user_enter_incorrect_details_to_login_xiii() throws Throwable {
-		WebElement username = wait.until(ExpectedConditions.elementToBeClickable(By.id("email")));
+		WebElement username = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='email']")));
 		Thread.sleep(2000);
 		username.sendKeys("*&#@Y #RHFGHJEGFYUJEh");
 		Thread.sleep(2000);
 		
-		WebElement password = wait.until(ExpectedConditions.elementToBeClickable(By.id("pass")));
+		WebElement password = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//fieldset[@class='fieldset login']//input[@id='pass']")));
 		Thread.sleep(2000);
 		password.sendKeys("jhjhddhjdhdfhfhfh");
 		Thread.sleep(2000);
 		
-		WebElement login_btn = wait.until(ExpectedConditions.elementToBeClickable(By.id("send2")));
+		WebElement login_btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//fieldset[@class='fieldset login']//button[@id='send2']")));
 		Thread.sleep(2000);
 		login_btn.click();
 		Thread.sleep(2000);
@@ -60,12 +61,12 @@ public class pdp_Email_Login_Incorrect_Data_Free_User_13 extends Set {
 	@Then("^error message is displayed xiii$")
 	public void error_message_is_displayed_xiii() throws Throwable {
 		
-		WebElement username = wait.until(ExpectedConditions.elementToBeClickable(By.id("email")));
+		WebElement username = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='email']")));
 		Thread.sleep(2000);
 		username.clear();
 		Thread.sleep(2000);
 		
-		WebElement password = wait.until(ExpectedConditions.elementToBeClickable(By.id("pass")));
+		WebElement password = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//fieldset[@class='fieldset login']//input[@id='pass']")));
 		Thread.sleep(2000);
 		password.clear();
 		Thread.sleep(2000);
@@ -76,17 +77,17 @@ public class pdp_Email_Login_Incorrect_Data_Free_User_13 extends Set {
 
 	@Then("^user login with correct details xiii$")
 	public void user_login_with_correct_details_xiii() throws Throwable {
-		WebElement username = wait.until(ExpectedConditions.elementToBeClickable(By.id("email")));
+		WebElement username = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='email']")));
 		Thread.sleep(2000);
 		username.sendKeys("slideteam.qa@yahoo.com");
 		Thread.sleep(2000);
 		
-		WebElement password = wait.until(ExpectedConditions.elementToBeClickable(By.id("pass")));
+		WebElement password = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//fieldset[@class='fieldset login']//input[@id='pass']")));
 		Thread.sleep(2000);
 		password.sendKeys("123456");
 		Thread.sleep(2000);
 		
-		WebElement login_btn = wait.until(ExpectedConditions.elementToBeClickable(By.id("send2")));
+		WebElement login_btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//fieldset[@class='fieldset login']//button[@id='send2']")));
 		Thread.sleep(2000);
 		login_btn.click();
 		Thread.sleep(2000);
@@ -116,11 +117,17 @@ public class pdp_Email_Login_Incorrect_Data_Free_User_13 extends Set {
 				Thread.sleep(2000);
 			}
 		} catch (NoSuchElementException Ext) {
-			Thread.sleep(2000);
+			
 			
 
 		}
-		
+		Thread.sleep(2000);
+		String verifySignout = wait
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//h3[@class='base']")))
+				.getText();
+		System.out.println("verifySignout = " + verifySignout);
+
+		Assert.assertTrue("Your are not Signout from application ", verifySignout.contentEquals("YOU ARE NOW LOGGED OUT"));
 	}
 
 	
