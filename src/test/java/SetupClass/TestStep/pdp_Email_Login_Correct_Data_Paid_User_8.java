9676c8863cfd676deab728508d6a0774970da332
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-12-01 14:37:47 +0530

    Update pdp_Email_Login_Correct_Data_Paid_User_8.java

--- src/test/java/SetupClass/TestStep/pdp_Email_Login_Correct_Data_Paid_User_8.java
+++ src/test/java/SetupClass/TestStep/pdp_Email_Login_Correct_Data_Paid_User_8.java
@@ -43,7 +43,9 @@ public class pdp_Email_Login_Correct_Data_Paid_User_8 extends Set {
 
 	@Then("^user is redirected to Login page viii$")
 	public void user_is_redirected_to_Login_page_viii() throws Throwable {
-	   Thread.sleep(3000);
+	   Thread.sleep(2000);
+              driver.get("https://www.slideteam.net/customer/account/login/");
+		Thread.sleep(2000);
 	}
 
 	@Then("^user login with correct details viii$")
