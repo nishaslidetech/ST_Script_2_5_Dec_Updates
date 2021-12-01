d8dad4140da8e6e2c13053e225f880bd42ef9d61
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-12-01 18:10:56 +0530

    Update pdp_Facebook_Login_Paid_User_9.java

--- src/test/java/SetupClass/TestStep/pdp_Facebook_Login_Paid_User_9.java
+++ src/test/java/SetupClass/TestStep/pdp_Facebook_Login_Paid_User_9.java
@@ -94,12 +94,7 @@ public class pdp_Facebook_Login_Paid_User_9 extends Set {
 		} catch (NoSuchElementException Ext) {
 
 		}
-		String verifySignout = wait
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//h3[@class='base']")))
-				.getText();
-		System.out.println("verifySignout = " + verifySignout);
-
-		Assert.assertTrue("Your are not Signout from application ", verifySignout.contentEquals("YOU ARE NOW LOGGED OUT"));
+		
 	}
 
 	
